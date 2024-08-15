684c9851af55672d607781684674f7c3d2740b63
martigonchi <martigonchi@gmail.com>
2024-08-15 20:36:51 -0300

    Create eventos.js

---
+++ eventos.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const div = document.getElementById('myDiv');
+    div.addEventListener('click', function() {
+        alert('Hola! Soy el div');
+    });
+});
